2bf7c0adf6cfce4b6ae2c1ea0ca09250a96a9181
Perizat Ibraeva <perizat.ibraeva.k@gmail.com>
2023-03-26 21:58:17 +0300

    add comments

--- first-assignment/app.js
+++ first-assignment/app.js
@@ -1,17 +1,25 @@
 import axios from "axios";
 
+// An anonymous function, which takes user ID as a parameter.
 export default (user_id) => {
   return new Promise(async (resolve, reject) => {
-    if (typeof user_id != "number") reject("Please correct the user ID.");
+    // Reject the promise if the entered user ID is not a number
+    if (typeof user_id != "number") reject("The user ID should be a number.");
+
+    // Retrieve user data with axios as user with specified user ID
     const { data: user } = await axios(
       `https://jsonplaceholder.typicode.com/users/${user_id}`
     );
 
+    // Retrieve posts data of specified user with axios as posts
     const { data: posts } = await axios(
       `https://jsonplaceholder.typicode.com/posts?userId=${user_id}`
     );
 
+    // Add retrieved posts to the user object
     user.posts = posts;
+
+    // Resolve the promise with user data
     resolve(user);
   });
 };
